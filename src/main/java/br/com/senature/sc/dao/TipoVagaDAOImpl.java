5e20f5554e22c8d637bf4cd11a4ed3fd00bce16c
Renan <48733894+renanluz@users.noreply.github.com>
2020-02-27 18:02:43 -0300

    Update TipoVagaDAOImpl

--- src/main/java/br/com/senature/sc/dao/TipoVagaDAOImpl.java
+++ src/main/java/br/com/senature/sc/dao/TipoVagaDAOImpl.java
@@ -2,8 +2,11 @@ package br.com.senature.sc.dao;
 
 import java.util.List;
 
+import javax.ejb.Stateless;
+
 import br.com.senature.sc.entity.TipoVaga;
 
+@Stateless
 public class TipoVagaDAOImpl extends GenericDAOImpl<TipoVaga, Integer> implements TipoVagaDAO{
 
 	public TipoVagaDAOImpl() {
