e8bc9e1488b25aa9e45e4ae58d85bf7d0f45634d
Renan <48733894+renanluz@users.noreply.github.com>
2020-02-27 17:48:40 -0300

    Create class TipoVagaDAOImpl

---
+++ src/main/java/br/com/senature/sc/dao/TipoVagaDAOImpl.java
@@ -0,0 +1,17 @@
+package br.com.senature.sc.dao;
+
+import java.util.List;
+
+import br.com.senature.sc.entity.TipoVaga;
+
+public class TipoVagaDAOImpl extends GenericDAOImpl<TipoVaga, Integer>{
+
+	public TipoVagaDAOImpl() {
+		super();
+	}
+	
+	public List<TipoVaga> buscarPorNome(String nome) {
+		return em.createQuery("from TipoVaga t where t.nome like :n", TipoVaga.class)
+				.setParameter("n", "%" + nome + "%").getResultList();
+	}
+}
